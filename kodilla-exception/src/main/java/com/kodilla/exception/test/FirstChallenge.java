24f5ac4440be2961667eea6c633e3e72b0d0595a
am4nto <luigii.lui@gmail.com>
2018-02-25 19:02:18 +0100

    8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class FirstChallenge {
         public double divide ( double a, double b) throws ArithmeticException {
-
                 if (b == 0) {throw new ArithmeticException();}
                 return a / b;
         }
