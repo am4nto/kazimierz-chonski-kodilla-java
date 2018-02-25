e896a5ee0cebf9d8a857f6662466eee714514db3
am4nto <luigii.lui@gmail.com>
2018-02-25 18:59:01 +0100

    8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -8,21 +8,18 @@ public class FirstChallenge {
 
                 if (b == 0) {throw new ArithmeticException();}
                 return a / b;
-
-
         }
 
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
         try {
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
         }catch (ArithmeticException e){
             System.out.println("Nie dzieli się przez zero! " + e);
         }finally {
             System.out.println("Do widzenia");
         }
-
     }
 }
