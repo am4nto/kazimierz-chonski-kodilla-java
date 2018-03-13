702423c81ec24a570b8aa762d9151b8dd03c9e8f
am4nto <luigii.lui@gmail.com>
2018-03-13 17:56:57 +0100

    10.2

--- kodilla_spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla_spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,10 +1,8 @@
 package com.kodilla.spring.calculator;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
