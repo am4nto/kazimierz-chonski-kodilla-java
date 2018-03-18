39b58c19c3990067c86e235f964e19f6f92908b6
am4nto <luigii.lui@gmail.com>
2018-03-18 10:50:18 +0100

    10.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/zadanie/Main.java
@@ -0,0 +1,4 @@
+package com.kodilla.stream.zadanie;
+
+public class Main {
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/zadanie/TestMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.zadanie;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Test {
+    
+}
