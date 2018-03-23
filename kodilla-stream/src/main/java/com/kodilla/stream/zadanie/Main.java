26cbcb585c62a7b20591426a90a6915e46cb32be
am4nto <luigii.lui@gmail.com>
2018-03-23 15:16:38 +0100

    ...

--- kodilla-stream/src/main/java/com/kodilla/stream/zadanie/Main.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/zadanie/Main.java
@@ -1,4 +1,48 @@
 package com.kodilla.stream.zadanie;
 
+import java.util.*;
+
 public class Main {
+    public static long getCountEmptyStringUsingJava8(List<String> strings) {
+        long count = strings.stream()
+                .filter(s -> s.isEmpty())
+                .count();
+        return count;
+    }
+
+    public static long getCountLength3UsingJava8(List<String> strings) {
+        long count = strings.stream()
+                .filter(s -> s.length() == 3)
+                .count();
+        return count;
+    }
+
+    public static List<String> deleteEmptyStringsUsingJava8(List<String> strings) {
+        return new ArrayList<>();
+    }
+
+    public static String getMergedStringUsingJava8(List<String> strings, String string) {
+        return "";
+    }
+
+    public static List<Integer> getSquaresJava8(List<Integer> numbers) {
+        return new ArrayList<Integer>();
+    }
+
+    public static Integer getMaxJava8(List<Integer> numbers) {
+        return 0;
+    }
+
+    public static Integer getMinJava8(List<Integer> numbers) {
+        return 0;
+    }
+
+
+    public static Integer getSumJava8(List<Integer> numbers) {
+        return 0;
+    }
+
+    public static Integer getAverageJava8(List<Integer> numbers) {
+        return 0;
+    }
 }
