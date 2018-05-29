4089d0973edac30181565742b142768366cdc717
am4nto <luigii.lui@gmail.com>
2018-05-29 14:13:01 +0200

    20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/dao/calculator/Watcher.java
@@ -0,0 +1,58 @@
+package com.kodilla.patterns2.dao.calculator;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Aspect
+@Component
+public class Watcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))" +
+            "&& args(theNumber) && target(object)")
+    public void logEvent(BigDecimal theNumber, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + theNumber);
+    }
+    @Around("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("Processing order");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureProcessingTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        long begin = System.currentTimeMillis();
+        try {
+            result = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        } finally {
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + "[ms]");
+        }
+        return result;
+    }
+}
